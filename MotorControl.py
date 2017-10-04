bd12e9a5cfdf87d8532d2e5589e6da37ffa31628
Paolo <paolodekeyzer@gmail.com>
2017-10-04 11:25:08 +0200

    MotorControlv3

--- MotorControl.py
+++ MotorControl.py
@@ -3,33 +3,33 @@ from time import sleep
 
 GPIO.setmode(GPIO.BOARD)
 
-Motor1Arr = 16
-Motor1Av = 18
+Motor1A = 16
+Motor1B = 18
 Motor1E = 22
 Motor2E = 15
-Motor2Arr = 13
-Motor2Av = 11
+Motor2A = 13
+Motor2B = 11
 
-GPIO.setup(Motor1Arr, GPIO.OUT)
-GPIO.setup(Motor1Av, GPIO.OUT)
+GPIO.setup(Motor1A, GPIO.OUT)
+GPIO.setup(Motor1B, GPIO.OUT)
 GPIO.setup(Motor1E, GPIO.OUT)
-GPIO.setup(Motor2Arr, GPIO.OUT)
-GPIO.setup(Motor2Av, GPIO.OUT)
+GPIO.setup(Motor2A, GPIO.OUT)
+GPIO.setup(Motor2B, GPIO.OUT)
 GPIO.setup(Motor2E, GPIO.OUT)
 
 print("Turning motor on")
-GPIO.output(Motor1Arr, GPIO.HIGH)
-GPIO.output(Motor1Av, GPIO.LOW)
+GPIO.output(Motor1A, GPIO.HIGH)
+GPIO.output(Motor1B, GPIO.LOW)
 GPIO.output(Motor1E, GPIO.HIGH)
-GPIO.output(Motor2Arr, GPIO.HIGH)
-GPIO.output(Motor2Av, GPIO.LOW)
+GPIO.output(Motor2A, GPIO.HIGH)
+GPIO.output(Motor2B, GPIO.LOW)
 GPIO.output(Motor2E, GPIO.HIGH)
 
 sleep(2)
 
 print("Stopping motor")
 GPIO.output(Motor1E, GPIO.LOW)
+GPIO.output(Motor2A, GPIO.LOW)
 GPIO.output(Motor2E, GPIO.LOW)
-GPIO.output(Motor2Arr, GPIO.LOW)
 
 GPIO.cleanup()
