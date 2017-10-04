c6d905dd10200ab7ce479bdec86184a808faaa7b
Paolo <paolodekeyzer@gmail.com>
2017-10-04 11:09:37 +0200

    MotorControlv2.9.5

--- MotorControl.py
+++ MotorControl.py
@@ -8,10 +8,12 @@ Motor1B = 18
 Motor1E = 22
 Motor2E = 15
 Motor2A = 13
+Motor2B = 11
 
 GPIO.setup(Motor1A, GPIO.OUT)
 GPIO.setup(Motor1B, GPIO.OUT)
 GPIO.setup(Motor1E, GPIO.OUT)
+GPIO.setup(Motor2A, GPIO.OUT)
 GPIO.setup(Motor2A, GPIO.OUT)
 GPIO.setup(Motor2E, GPIO.OUT)
 
@@ -20,6 +22,7 @@ GPIO.output(Motor1A, GPIO.HIGH)
 GPIO.output(Motor1B, GPIO.LOW)
 GPIO.output(Motor1E, GPIO.HIGH)
 GPIO.output(Motor2A, GPIO.LOW)
+GPIO.output(Motor2B, GPIO.LOW)
 GPIO.output(Motor2E, GPIO.HIGH)
 
 sleep(2)
