783427f81f9ce5aa6e2418cb996b8be1ad15a145
Paolo <paolodekeyzer@gmail.com>
2017-10-04 10:41:07 +0200

    MotorControlv2.5

--- MotorControl.py
+++ MotorControl.py
@@ -19,7 +19,7 @@ print("Turning motor on")
 GPIO.output(Motor1A, GPIO.HIGH)
 GPIO.output(Motor1B, GPIO.LOW)
 GPIO.output(Motor1E, GPIO.HIGH)
-GPIO.output(Motor2A, GPIO.HIGH)
+GPIO.output(Motor2A, GPIO.LOW)
 GPIO.output(Motor2E, GPIO.HIGH)
 
 sleep(2)
