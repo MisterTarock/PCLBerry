dd502e9f78ffbafb88238615b088a8d3d17f9e7f
Paolo <paolodekeyzer@gmail.com>
2017-10-04 10:33:50 +0200

    MotorControlv2.1

--- MotorControl.py
+++ MotorControl.py
@@ -6,19 +6,26 @@ GPIO.setmode(GPIO.BOARD)
 Motor1A = 16
 Motor1B = 18
 Motor1E = 22
+Motor2E = 11
+Motor2A = 10
 
 GPIO.setup(Motor1A, GPIO.OUT)
 GPIO.setup(Motor1B, GPIO.OUT)
 GPIO.setup(Motor1E, GPIO.OUT)
+GPIO.setup(Motor2A, GPIO.OUT)
+GPIO.setup(Motor2E, GPIO.OUT)
 
 print("Turning motor on")
 GPIO.output(Motor1A, GPIO.HIGH)
 GPIO.output(Motor1B, GPIO.LOW)
 GPIO.output(Motor1E, GPIO.HIGH)
+GPIO.output(Motor2A, GPIO.LOW)
+GPIO.output(Motor2E, GPIO.HIGH)
 
 sleep(2)
 
 print("Stopping motor")
 GPIO.output(Motor1E, GPIO.LOW)
+GPIO.output(Motor2E, GPIO.LOW)
 
 GPIO.cleanup()
