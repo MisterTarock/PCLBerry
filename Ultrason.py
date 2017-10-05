e0df4cd18cf9165c07a406c30b021e1eb1135dd2
MisterTarock <victor_pb@hotmail.fr>
2017-10-05 17:22:55 +0200

    Ultra2.1.1

--- Ultrason.py
+++ Ultrason.py
@@ -1,5 +1,5 @@
 import RPi.GPIO as GPIO
-from time import sleep
+import time
 
 GPIO.setmode(GPIO.BOARD)
 
@@ -13,17 +13,17 @@ GPIO.setup(ECHO,GPIO.IN)
 
 GPIO.output(TRIG, False)
 print "Waiting For Sensor To Settle"
-sleep(2)
+time.sleep(2)
 
 GPIO.output(TRIG, True)
-sleep(0.00001)
+time.sleep(0.00001)
 GPIO.output(TRIG, False)
 
 while GPIO.input(ECHO)==0:
-  pulse_start = time()
+  pulse_start = time.time()
 
 while GPIO.input(ECHO)==1:
-  pulse_end = time()
+  pulse_end = time.time()
 
 pulse_duration = pulse_end - pulse_start #to mesure the time of the ECHO
 
