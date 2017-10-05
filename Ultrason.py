b038d419bbab00fc519ddf556881bfb6037480c5
MisterTarock <victor_pb@hotmail.fr>
2017-10-05 17:16:02 +0200

    Ultra2.0

--- Ultrason.py
+++ Ultrason.py
@@ -3,7 +3,33 @@ from time import sleep
 
 GPIO.setmode(GPIO.BOARD)
 
-Trig= 26
-Echo= 24
+TRIG= 26  #to send the ultrasonic pulse
+ECHO= 24  #to mesure the distance from the obstacle and give it as a 5V impluse
+
+print "Distance Measurement In Progress"
+
+GPIO.setup(TRIG,GPIO.OUT)
+GPIO.setup(ECHO,GPIO.IN)
+
+GPIO.output(TRIG, False)
+print "Waiting For Sensor To Settle"
+time.sleep(2)
+
+GPIO.output(TRIG, True)
+time.sleep(0.00001)
+GPIO.output(TRIG, False)
+
+while GPIO.input(ECHO)==0:
+  pulse_start = time.time()
+
+while GPIO.input(ECHO)==1:
+  pulse_end = time.time()
+
+pulse_duration = pulse_end - pulse_start #to mesure the time of the ECHO
+
+distance = pulse_duration x 17150
+distance = round(distance, 2)
+
+print "Distance:",distance,"cm"
 
 GPIO.cleanup()
