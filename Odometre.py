b407287caf9f0931c9a9df1cd3173140387cf601
Paolo <paolodekeyzer@gmail.com>
2017-10-11 09:18:27 +0200

    odom1

--- Odometre.py
+++ Odometre.py
@@ -1,42 +1,45 @@
 import RPi.GPIO as GPIO
 import time
 #to define whicj type of layout is used for the pin mapping
-GPIO.setmode(GPIO.BOARD)
 
-OdoL= 23
-OdoD= 21
+class Odo:
+    def __init__(self):
+        GPIO.setmode(GPIO.BOARD)
 
-GPIO.setup(OdoD, GPIO.IN)
-GPIO.setup(OdoL, GPIO.IN)
-L=0
-def incrementL(Lnow):
-    L=Lnow
-    if GPIO.input(OdoL):     # if port 23 == 1
-        print ("Rising edge detected on OdoL")
-        L=L+1
-        print (L)
-    else:                  # if port 23 != 1
-        print ("Falling edge detected on OdoL")
+        OdoL = 23
+        OdoD = 21
 
-def incrementD(Dnow):
-    D=Dnow
-    if GPIO.input(OdoD):     # if port 21 == 1
-        print ("Rising edge detected on OdoD")
-    else:                  # if port 21 != 1
-        print ("Falling edge detected on OdoD")
+        GPIO.setup(OdoD, GPIO.IN)
+        GPIO.setup(OdoL, GPIO.IN)
+        self.L = 0
+        self.Acquisition()
 
-def Acquisition(L,D):
-    Lnow=L
-    Dnow=D
-    print('Acquisition')
-    GPIO.add_event_detect(OdoL, GPIO.RISING, callback=incrementL(Lnow))
-    #GPIO.add_event_detect(OdoL, GPIO.FALLING, callback=incrementL)
 
-    GPIO.add_event_detect(OdoD, GPIO.RISING, callback=incrementD(Dnow))
-    #GPIO.add_event_detect(OdoD, GPIO.FALLING, callback=incrementD)
-L=0
-D=0
-Acquisition(L,D)
+    def incrementL(self):
+        if GPIO.input(self.OdoL):     # if port 23 == 1
+            print ("Rising edge detected on OdoL")
+            self.L+=1
+            print (self.L)
+        else:                  # if port 23 != 1
+            print ("Falling edge detected on OdoL")
+
+    def incrementD(self):
+
+        if GPIO.input(self.OdoD):     # if port 21 == 1
+            print ("Rising edge detected on OdoD")
+        else:                  # if port 21 != 1
+            print ("Falling edge detected on OdoD")
+
+    def Acquisition(self):
+        Lnow=L
+        Dnow=D
+        print('Acquisition')
+        GPIO.add_event_detect(self.OdoL, GPIO.RISING, callback=self.incrementL)
+        #GPIO.add_event_detect(OdoL, GPIO.FALLING, callback=incrementL)
+
+        GPIO.add_event_detect(self.OdoD, GPIO.RISING, callback=self.incrementD)
+        #GPIO.add_event_detect(OdoD, GPIO.FALLING, callback=incrementD)
+Odo()
 time.sleep(2)
 
 
