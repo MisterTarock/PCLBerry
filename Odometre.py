cb8a8ee7e127dfe74121338116a627f0adb8e598
Paolo <paolodekeyzer@gmail.com>
2017-10-11 11:14:55 +0200

    odom5

--- Odometre.py
+++ Odometre.py
@@ -47,7 +47,7 @@ class Odo:
             GPIO.cleanup()
             return 1
         print('Acquisition')
-        GPIO.add_event_detect(self.OdoL, GPIO.RISING, callback=self.incrementL, bouncetime=100)
+        GPIO.add_event_detect(self.OdoL, GPIO.BOTH, callback=self.incrementL, bouncetime=100)
         #GPIO.add_event_detect(OdoL, GPIO.FALLING, callback=incrementL)
 
         #GPIO.add_event_detect(self.OdoD, GPIO.BOTH, callback=self.incrementD, bouncetime=100)
