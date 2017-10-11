c908d27c51b7d879a6c401d88bba686c7f18445e
Paolo <paolodekeyzer@gmail.com>
2017-10-11 09:30:27 +0200

    odom3

--- Odometre.py
+++ Odometre.py
@@ -33,7 +33,7 @@ class Odo:
     def Acquisition(self):
 
         print('Acquisition')
-        GPIO.add_event_detect(self.OdoL, GPIO.RISING, callback=self.incrementL())
+        GPIO.add_event_detect(self.OdoL, GPIO.RISING, callback=self.incrementL)
         #GPIO.add_event_detect(OdoL, GPIO.FALLING, callback=incrementL)
 
         GPIO.add_event_detect(self.OdoD, GPIO.RISING, callback=self.incrementD())
