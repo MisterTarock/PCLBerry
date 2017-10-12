1d2bb8c19a3b4ddc8cef4faa0502b4742cfb8aa0
Paolo <paolodekeyzer@gmail.com>
2017-10-12 13:30:15 +0200

    Fixing wheels speed

--- MotorControl.py
+++ MotorControl.py
@@ -28,7 +28,7 @@ class MotorControl:
         print("Turning motor Forward")
 
         self.M1Bac.ChangeDutyCycle(0)
-        self.M1For.ChangeDutyCycle(0.5)
+        self.M1For.ChangeDutyCycle(1)
         self.M2Bac.ChangeDutyCycle(0)
         self.M2For.ChangeDutyCycle(1)
         # GPIO.output(self.Motor1Arr, GPIO.LOW)
