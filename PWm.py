592fd72ab6927eb6b5c2ea0fb093ffbd3828d1d9
Paolo <paolodekeyzer@gmail.com>
2017-10-17 15:29:21 +0200

    zoierfjoez

--- PWm.py
+++ PWm.py
@@ -1,6 +1,12 @@
 import RPi.GPIO as GPIO
+import time
 GPIO.setmode(GPIO.BOARD)
 Motor1Av = 18
 GPIO.setup(Motor1Av, GPIO.OUT)
 M1For = GPIO.PWM(Motor1Av, 100)
 M1For.ChangeDutyCycle(50)
+time.sleep(2)
+M1For.ChangeDutyCycle(100)
+
+time.sleep(2)
+M1For.ChangeDutyCycle(0)
