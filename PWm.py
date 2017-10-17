743be53af7411fac82513b44388f3a880b70a0b9
Paolo <paolodekeyzer@gmail.com>
2017-10-17 16:03:36 +0200

    zoierfjoez

--- PWm.py
+++ PWm.py
@@ -3,8 +3,14 @@ import time
 GPIO.setmode(GPIO.BOARD)
 Motor1Av = 18
 Motor2Av = 12
+Motor1E = 22
+Motor2E = 15
 GPIO.setup(Motor1Av, GPIO.OUT)
 GPIO.setup(Motor2Av, GPIO.OUT)
+GPIO.setup(Motor1E, GPIO.OUT)
+GPIO.setup(Motor2E, GPIO.OUT)
+GPIO.output(Motor1E, GPIO.HIGH)
+GPIO.output(Motor2E, GPIO.HIGH)
 M1For = GPIO.PWM(Motor1Av, 100)
 M2For = GPIO.PWM(Motor2Av, 100)
 print("turning on")
