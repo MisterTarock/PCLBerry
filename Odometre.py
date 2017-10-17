20af529537da4c9e5302f68e3368867a28e2062f
Paolo <paolodekeyzer@gmail.com>
2017-10-17 16:50:45 +0200

    zoierfjoez

--- Odometre.py
+++ Odometre.py
@@ -26,6 +26,7 @@ class Odo:
         self.sensor=Ultrason()
         self.motor=MotorControl()
         self.motor.forward(90,self.PWMD)
+        self.motor.forward(50, 50)
         self.Acquisition()
 
 
